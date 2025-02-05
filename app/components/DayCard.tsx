0a6c89b4f5755b0215760bf98c5677acd3ad6150
Serhat Karaman <90270116+MasterofN0ne@users.noreply.github.com>
2025-02-05 20:40:16 +0300

    main design completed

--- app/components/DayCard.tsx
+++ app/components/DayCard.tsx
@@ -4,24 +4,28 @@ interface Props extends React.PropsWithChildren {
     className?: string;
     isActive?: boolean;
     onClick?: () => void;
-    date?: number;
-    key?: number;
-    weekly?: boolean;
+    day?: string;
+    month?: string;
+    totalDayIndex?: number;
+    key?: string;
   }
   const DayCard: React.FC<Props> = ({
     onClick,
     children,
     className = "",
-    date,
-    weekly,
+    totalDayIndex,
+    day,
+    month,
     isActive = false,
   }) => {
-    const isLeftmostCell = (date && date % 7 === 1);
+    const isLeftmostCell = day ? Number(day) % 10 === 1 : false;
     return (
       <div
-        className={`border-r border-b ${isLeftmostCell ? 'border-l' : ''} ${weekly ? 'border-l' : ''} border-black p-4 min-h-[120px] hover:bg-gray-50 transition-colors`}
+        className={`relative border-r border-b ${isLeftmostCell ? 'border-l' : ''}  border-black p-4 min-h-[120px] hover:bg-green-500 transition-colors ${className}`}
       >
-        <p>{date}</p>
+        <p className="absolute top-0 left-0 pl-2">{day}</p>
+        <p className="absolute bottom-0 left-0 pl-2">{month}</p>
+        <p className="absolute  bottom-0 right-0 pr-2">{totalDayIndex}</p>
       </div>
     );
   };
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 
-import MonthlyView from "./components/MonthlyView";
-import WeeklyView from "./components/WeeklyView";
+import CalendarComponent from "./components/CalendarComponent";
+
 
 export default function Home() {
   return (
-    <div className="flex h-full w-full">
-      <div className="flex border-1 h-screen w-1/4 bg-white text-white items-center justify-center">
-        <h1 className="text-center text-black">January</h1>
-      </div> 
-        <MonthlyView />        
+    <div className="flex h-full w-full"> 
+        <CalendarComponent />        
     </div>
   );
 }
